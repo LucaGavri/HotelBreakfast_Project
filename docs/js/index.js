8905e4ebdc4310b2284ef30b6992b0941946c63e
Lukasz Gawrys <lukaszgawrys13@gmail.com>
2020-07-28 22:04:50 +0200

    quic

--- docs/js/index.js
+++ docs/js/index.js
@@ -1,17 +1,21 @@
 //log
-alert('login:Room513   pass:513');
+// alert('login:Room513   pass:513');
 
 const loginInput = document.querySelector('.login');
 const passInput = document.querySelector('.pass');
 const loginBtn = document.querySelector('.logBtn');
 
+// loginBtn.addEventListener('click', function(e){
+//     e.preventDefault();
+//     if(loginInput.value === 'Room513' && passInput.value === '513'){
+//         window.location = './HBsite.html'
+//     }
+//     else {
+//         alert('Wrong login or password. Please try again')
+//     }
+// });
+
 loginBtn.addEventListener('click', function(e){
     e.preventDefault();
-    if(loginInput.value === 'Room513' && passInput.value === '513'){
-        window.location = './HBsite.html'
-    }
-    else {
-        alert('Wrong login or password. Please try again')
-    }
+    window.location = './HBsite.html'
 });
-
